722254f23fdfaddd4a69697559138dd0baf56443
22felipefdev <felipe.ferneda20@gmail.com>
2026-09-05 02:22:39 -0300

    style: aplica terraform fmt recursivo para ajustar formatacao

--- 01-bootstrap/main.tf
+++ 01-bootstrap/main.tf
@@ -6,38 +6,38 @@ terraform {
     }
   }
 
-   backend "s3" {
-	bucket = "meu-bucket-tfstate-local"
-	key    = "01-bootstrap/terraform.tfstate"
-	region = "us-east-1"
-	dynamodb_table = "meu-lock-table-local"
-	endpoints = { 
-	s3 = "http://localhost:4566"
-	dynamodb = "http://localhost:4566"
-	sts = "http://localhost:4566"
-	}
-	use_path_style = true
-	skip_credentials_validation = true
-	skip_metadata_api_check = true
-   	skip_region_validation = true
-	skip_requesting_account_id = true
- }
+  backend "s3" {
+    bucket         = "meu-bucket-tfstate-local"
+    key            = "01-bootstrap/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "meu-lock-table-local"
+    endpoints = {
+      s3       = "http://localhost:4566"
+      dynamodb = "http://localhost:4566"
+      sts      = "http://localhost:4566"
+    }
+    use_path_style              = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+  }
 }
 
 provider "aws" {
-  region = "us-east-1"
-  access_key = "test"
-  secret_key = "test"
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
   skip_credentials_validation = true
-  skip_metadata_api_check = true
-  skip_requesting_account_id = true
-  s3_use_path_style = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  s3_use_path_style           = true
 
   endpoints {
-    s3 = "http://localhost:4566"
+    s3       = "http://localhost:4566"
     dynamodb = "http://localhost:4566"
-    sts = "http://localhost:4566"
-    iam = "http://localhost:4566"
+    sts      = "http://localhost:4566"
+    iam      = "http://localhost:4566"
   }
 }
 
